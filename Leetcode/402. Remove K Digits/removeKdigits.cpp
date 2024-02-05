782cdb125a9631dead965a390c9dd2c7b6c998ba
hieuvd341 <vuduchieu1611@gmail.com>
2024-02-05 14:47:52 +0700

    add Remove K Digits solution

---
+++ Leetcode/402. Remove K Digits/removeKdigits.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include<stack>
+#include<algorithm>
+#include <Windows.h>
+using namespace std;
+string removeKdigits(string num, int k)
+{
+    int size = num.size();
+    stack<char> s;
+    s.push(num[0]);
+    for (int i = 1; i < size; i++)
+    {
+        while (!s.empty() && num[i] < s.top() && k > 0)
+        {
+            s.pop();
+            k--;
+        }
+        if (!s.empty() || num[i] != '0')
+        {
+            s.push(num[i]);
+        }
+    }
+    while (k-- > 0 && !s.empty())
+    {
+        s.pop();
+    }
+    string res;
+    while (!s.empty())
+    {
+        res += s.top();
+        s.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res == "" ? "0" : res;
+}
+int main()
+{
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open())
+    {
+        cerr << "Can not open file";
+        return 1;
+    }
+    string s;
+    int k;
+    inputFile >> s;
+    inputFile >> k;
+    cout << removeKdigits(s, k);
+}
