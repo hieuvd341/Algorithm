d93c8a4d8918dbdd1bf8cbc458642e945b174de7
hieuvd341 <vuduchieu1611@gmail.com>
2024-02-05 12:56:48 +0700

    add Valid Triangle Number solution

---
+++ Leetcode/611. Valid Triangle Number/triangleNumber.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <map>
+#include <Windows.h>
+using namespace std;
+int triangleNumber(vector<int> &nums)
+{
+    sort(begin(nums), end(nums));
+    int lb;
+    int count = 0;
+    int ni = nums.size() - 2;
+    int nj = nums.size() - 1;
+    for (int i = 0; i < ni; i++)
+    {
+        for (int j = i + 1; j < nj; j++)
+        {
+            lb = lower_bound(nums.begin() + j + 1, nums.end(), nums[i] + nums[j]) - nums.begin();
+            count += lb - j - 1;
+        }
+    }
+    return count;
+}
+int main()
+{
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open())
+    {
+        cerr << "Can not open file";
+        return 1;
+    }
+    vector<int> nums;
+    int tmp;
+    while (!inputFile.eof())
+    {
+        inputFile >> tmp;
+        nums.push_back(tmp);
+    }
+    cout << triangleNumber(nums);
+}
