984153eb75c35652df846e772c586e3a6b7ed52a
hieuvd341 <vuduchieu1611@gmail.com>
2024-11-27 14:48:04 +0700

    apply change

--- Leetcode/179. Largest Number/LargestNumber.cpp
+++ Leetcode/179. Largest Number/LargestNumber.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -43,50 +42,4 @@ int main()
     cout << largestNumber(nums);
 
     return 0;
-=======
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <map>
-#include <Windows.h>
-#include <string>
-#include <algorithm>
-using namespace std;
-string largestNumber(vector<int> &nums)
-{
-    auto cmp = [&](int &a, int b)
-    {
-        return (to_string(a) + to_string(b) > to_string(b) + to_string(a));
-    };
-    sort(nums.begin(), nums.end(), cmp);
-    string ans = "";
-    for (auto it : nums)
-    {
-        ans += to_string(it);
-    }
-    return ans[0] == '0' ? "0" : ans;
-}
-int main()
-{
-    ifstream inputFile("input.txt");
-    if (!inputFile.is_open())
-    {
-        cerr << "Can not open file";
-        return 1;
-    }
-
-    vector<int> nums;
-    int tmp; 
-
-    while (inputFile >> tmp) 
-    {
-        nums.push_back(tmp);
-    }
-
-    inputFile.close(); // Close the input file after reading
-
-    cout << largestNumber(nums);
-
-    return 0;
->>>>>>> bad19b40af882feab18b89e58bb73dec7250f825
 }
--- hackerrank/Shortest Palindrome/ShortestPalindrome.cpp
+++ hackerrank/Shortest Palindrome/ShortestPalindrome.cpp
@@ -1,39 +1,38 @@
-#include<iostream>
-#include<map>
-#include<fstream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int solve(string s) {
-    int size = s.size();
-    map<char, int> mp;
-    for(int i =0; i < size; i++){
-        mp[s[i]] ++;
-    }
-    int dp[1100];
-    dp[0] = 0;
-    int max = 0;
-    int idx;
-    for(int i =1; i < size-1; i++ ) {
-        dp[i] += dp[i-1] + mp[i-1]-1;
-        mp[i-1] = mp[i-1]/2-1;
-        if(dp[i] > max) {
-            idx = i;
-            max = dp[i];
+// Function to find minimum insertions to form a palindrome
+int shortestPalindrome(string s) {
+    string rev_s = s;
+    reverse(rev_s.begin(), rev_s.end());
+    int n = s.size();
+
+    // Dynamic Programming Table for LCS
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    // Fill DP Table
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s[i - 1] == rev_s[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
     }
-    cout << idx << endl;
-    cout << max << endl;
-    return size - max*2;
 
+    // Calculate Minimum Insertions
+    int longest_palindromic_suffix = dp[n][n];
+    return n - longest_palindromic_suffix;
 }
+
 int main() {
-    ifstream inputFile("input.txt");
-    if(!inputFile.is_open()) {
-        cerr << "Can not open file.";
-        return 0;
-    }
     string s;
-    inputFile >> s;
-    cout << solve(s);
+    cout << "Enter the string: ";
+    cin >> s;
+
+    cout << "Minimum insertions needed: " << shortestPalindrome(s) << endl;
+    return 0;
 }
